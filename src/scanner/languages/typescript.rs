764c61b6aaf169e51986f5838f5b96953b833e15
sudipta-hd <sudipta@hounddog.ai>
2024-07-15 07:29:15 -0700

    Add identifier support for TypeScript/JavaScript

--- src/scanner/languages/typescript.rs
+++ src/scanner/languages/typescript.rs
@@ -2,13 +2,31 @@ use anyhow::Result;
 use tree_sitter::Node;
 use crate::enums::VisitChildren;
 use crate::scanner::languages::base::BaseScanner;
-use crate::structs::FileScanContext;
+use crate::structs::{DataElementOccurrence, FileScanContext};
 
 pub struct TypescriptScanner;
 
 impl BaseScanner for TypescriptScanner {
     fn visit_node(state: &mut FileScanContext, node: &Node) -> Result<VisitChildren> {
-        Ok(VisitChildren::No)
+        match (node.kind()) {
+            "identifier" => {}
+            "property_identifier"  => {
+                let text = state.get_node_text(node);
+                if let Some(data_element) = state.find_data_element(&text) {
+                    let _ = state.put_occurrence(DataElementOccurrence::from_node(
+                        state,
+                        node,
+                        &data_element,
+                    ));
+                    return Ok(VisitChildren::No); // Do not look at parts of the same attribute again.
+                }
+            }
+            "method_definition" => {}
+            "function_declaration" => {}
+            "type_identifier" => {}
+            _ => {}
+        }
+        Ok(VisitChildren::Yes)
     }
 
     fn leave_node(state: &mut FileScanContext, node: &Node) {}
---
+++ test/sampleinput.js
@@ -0,0 +1,27 @@
+// Define a class representing a Person
+class Person {
+    // Properties
+    private fullName: string;
+    private phoneNumber: string;
+    private ssn: string;
+
+    // Constructor
+    constructor(fullName: string, phoneNumber: string, ssn: string) {
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.ssn = ssn;
+    }
+
+    // Method to display person's information
+    displayInfo(): void {
+        console.log(`Name: ${this.fullName}`);
+        console.log(`Phone Number: ${this.phoneNumber}`);
+        console.log(`SSN: ${this.ssn}`);
+    }
+}
+
+// Create an instance of the Person class
+let person1 = new Person('John Doe', '123-456-7890', '123-45-6789');
+
+// Display person's information
+person1.displayInfo();
